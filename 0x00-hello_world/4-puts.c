bf1001c884756f8a135507f741ef5affbf450703
Techgirl-BI <racheal.onimisi@outlook.com>
2022-10-13 16:16:34 -0700

    puts .c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- prints "Programming is like building a multilingual puzzle
+ * @void: function argument
+ * Return: 0 if the code runs successfully. 1 otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+		return (0);
+}
