0f3c5e00e8103aeefc2492f1730ab5a83bbe541b
Techgirl-BI <racheal.onimisi@outlook.com>
2022-10-15 05:36:55 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints the text
+ *
+ * Return: Always 0.
+ */
 
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
