c812d2023b15d7d3ae8f72928fcca52a81b95f86
Techgirl-BI <racheal.onimisi@outlook.com>
2022-10-24 12:47:47 -0700

    done

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is digit
+ * @c: carrier variable
+ * Return: 1 if true, 0 if false
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	else
+		return (0);
+}
+
+Task 2 
+
+#include "main.h"
+
+/**
+ * mul - gives the product of a and b
+ * @a: variable
+ * @b: variable
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
